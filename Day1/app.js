e30f3aa989f0a57bb1db5405635a0d5a0da2db48
prathmeshgodbole <godbole611@gmail.com>
2020-09-12 17:48:21 +0530

    DOM manipulation dynamic row,column

---
+++ Day1/app.js
@@ -0,0 +1,33 @@
+console.log(document) // DOM
+
+
+let col_name = ['Name', 'Prize', 'Qty', 'Total']
+
+let row_name = ['Prathmesh', 'Parag', 'Vaibhav', 'Siddiq']
+
+// let Prize = ['10000', '9000', '8000', '7000']
+
+// let Qty = ['5', '4', '3', '2']
+
+let table = document.getElementById('table')
+
+// Add Columns
+for (let i = 0; i < col_name.length; i++) {
+    let td = document.createElement('td')
+    td.style.border = '1px solid black'
+    td.textContent = col_name[i]
+    table.appendChild(td)
+}
+
+// Add Names rows
+for (let i = 0; i < row_name.length; i++) {
+    let tr = document.createElement('tr')
+    tr.style.border = '1px solid red'
+    tr.textContent = row_name[i]
+    table.appendChild(tr)
+}
+
+
+
+//
+//homework day 1 - table with dynamic rows, columns name , price, qty and total
